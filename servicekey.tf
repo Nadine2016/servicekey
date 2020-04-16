43a23483f55fac54b85d0048f82e956e6c5c84ea
Nadine2016 <nspies@us.ibm.com>
2020-04-16 08:34:52 -0400

    Update servicekey.tf

--- servicekey.tf
+++ servicekey.tf
@@ -6,17 +6,17 @@ resource "ibm_resource_instance" "resource_instance" {
   name              = "tf-db"
   service           = "databases-for-etcd"
   plan              = "lite"
-  resource_group_id = data.ibm_resource_group.group.id
+  resource_group_id = "${data.ibm_resource_group.group.id}"
 }
 
 resource "ibm_resource_key" "resourceKey" {
   name                 = "tfkey"
   role                 = "Viewer"
-  resource_instance_id = data.ibm_resource_instance.resource_instance.id
+  resource_instance_id = "${data.ibm_resource_instance.resource_instance.id}"
 
   parameters {
 
-     service-endpoints: "private"
+     service-endpoints =  "private"
   
   }
 }
