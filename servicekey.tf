eb7f7b37118c9d9ec877079795cbb19089c6d456
Nadine2016 <nspies@us.ibm.com>
2020-04-16 08:32:24 -0400

    Create servicekey.tf

---
+++ servicekey.tf
@@ -0,0 +1,22 @@
+data "ibm_resource_group" "group" {
+  name = "default"
+}
+
+resource "ibm_resource_instance" "resource_instance" {
+  name              = "tf-db"
+  service           = "databases-for-etcd"
+  plan              = "lite"
+  resource_group_id = data.ibm_resource_group.group.id
+}
+
+resource "ibm_resource_key" "resourceKey" {
+  name                 = "tfkey"
+  role                 = "Viewer"
+  resource_instance_id = data.ibm_resource_instance.resource_instance.id
+
+  parameters {
+
+     service-endpoints: "private"
+  
+  }
+}
